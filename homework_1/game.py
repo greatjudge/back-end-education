404d5c269bf1783a09b93332dfe41bd43cdb1421
greatjudge <greatjudge0@gmail.com>
2021-10-17 16:38:20 +0400

    Wrote tests and refact

--- homework_1/game.py
+++ homework_1/game.py
@@ -1,12 +1,17 @@
 from itertools import cycle
+from typing import List
+
 from exceptions import PlayerInputError, SetMarkError, InterruptedGameError
 from users import TicTacPlayer
+
+
+Board = List[List[str]]
 
 
 class TicTacBoard:
     def __init__(self, size: int = 3):
         self.size = size
-        self._map = self._make_board()
+        self._map = self._make_board(self.size)
         self._layout = self._make_layout()
 
     def __str__(self):
@@ -15,68 +20,89 @@ class TicTacBoard:
             string += ' '.join(row) + '\n'
         return string.rstrip('\n')
 
-    def _make_board(self):
-        return [['_'] * self.size for _ in range(self.size)]
+    @staticmethod
+    def _make_board(size: int) -> Board:
+        return [['_'] * size for _ in range(size)]
 
     def _make_layout(self):
         string = ''
         for nrow in range(self.size):
-            string += ' '.join([str(nrow * self.size + ncol) for ncol in range(self.size)]) + '\n'
+            string += ' '.join([str(nrow * self.size + ncol)
+                                for ncol in range(self.size)]) + '\n'
         return string.rstrip('\n')
 
+    def _validate_indexes(self, row_number, column_number):
+        if not 0 <= int(row_number) < self.size:
+            raise IndexError(f'row_number must be less {self.size} and larger 0')
+        if not 0 <= int(column_number) < self.size:
+            raise IndexError(f'column_number must be less {self.size} and larger 0')
+
     def set_mark(self, row_number: int,
-                 column_number: int, mark):
+                 column_number: int, mark: str):
+        self._validate_indexes(row_number, column_number)
+        if not isinstance(mark, str):
+            raise SetMarkError
         if self._map[row_number][column_number] == '_':
             self._map[row_number][column_number] = mark
-            return True
         else:
-            raise SetMarkError(f'map[{row_number}][{column_number}] must be free, '
-                               f'not {self._map[row_number][column_number]}')
+            raise SetMarkError
 
     def check_win(self, row_number, column_number, mark):
-        vertical = all([self._map[i][column_number] == mark for i in range(self.size)])
-        horizontal = all([self._map[row_number][j] == mark for j in range(self.size)])
+        self._validate_indexes(row_number, column_number)
+        vertical = all((self._map[i][column_number] == mark for i in range(self.size)))
+        horizontal = all((self._map[row_number][j] == mark for j in range(self.size)))
         diagonal = False
-        if row_number == column_number:
-            diag1 = all([self._map[i][i] == mark for i in range(self.size)])
-            diag2 = all([self._map[i][self.size - i - 1] == mark for i in range(self.size)])
+        if any((row_number == column_number, # If diagonal
+               row_number == self.size - 1 - column_number,
+               column_number == self.size - 1 - row_number)) :
+            diag1 = all((self._map[i][i] == mark for i in range(self.size)))
+            diag2 = all((self._map[i][self.size - i - 1] == mark for i in range(self.size)))
             diagonal = any((diag1, diag2))
         return any((vertical,
                     horizontal,
                     diagonal))
 
-    def show_board(self):
+    def board(self):
         return str(self)
 
-    def show_extended_board(self):
+    def extended_board(self):
         string = ''
         for nrow, row in enumerate(self._map):
-            string += (' '.join(row) + '\t' +
-                       ' '.join([str(nrow * self.size + ncol) for ncol in range(self.size)]) + '\n')
+            string += (' '.join([str(nrow * self.size + ncol)
+                                for ncol in range(self.size)])
+                       + '\t' + ' '.join(row) + '\n')
         return string.rstrip()
-
-    def update_board(self):
-        self._map = self._make_board()
 
     @property
     def layout(self):
         return self._layout
 
+    def update_board(self):
+        self._map = self._make_board(self.size)
+
 
 class TicTacGame:
-    quit_code = 'q!'
-    
-    def __init__(self, first_player: TicTacPlayer, second_player: TicTacPlayer, size: int = 3):
-        self._gamefield = TicTacBoard(size)
-        self.player1 = first_player
-        self.player2 = second_player
+    quit_code = 'q'
 
-    def start_game(self):
+    def __init__(self, size: int = 3):
+        self._size = size
+        self._gamefield = TicTacBoard(self._size)
+
+    def show_instructions(self):
+        instructions = 'TicTak Game.\n First player: x, ' \
+                       'Second player: o. Enter the number to put the symbol\n'
+        quit_instructions = f' Type \'{self.quit_code}\' to quit\n'
+        print(instructions + quit_instructions)
+
+    def start_game(self, first_name: str, second_name: str):
         self._gamefield.update_board()
-        self._show_instructions()
-        players = cycle((self.player1, self.player2))
+        self.show_instructions()
+        self._game(TicTacPlayer(first_name, 'x'),
+                   TicTacPlayer(second_name, 'o'))
 
-        for i in range(self._gamefield.size ** 2):
+    def _game(self, player1, player2):
+        players = cycle((player1, player2))
+        for _ in range(self._gamefield.size ** 2):
             player = next(players)
             self._show_extended_board()
 
@@ -87,22 +113,23 @@ class TicTacGame:
                 break
 
             if self._gamefield.check_win(row, column, player.mark):
+                self._show_board()
                 self._show_greeting(player)
                 break
         else:
+            self._show_board()
             self._show_draw()
-        self._show_board()
 
     def _make_move(self, player):
         while True:
             try:
-                number = self._parse_input(input(f'{player.name}`s ({player.mark}) move: '))
+                number = self._parse_input(self._player_input(player))
                 row = number // self._gamefield.size
                 column = number % self._gamefield.size # т.к. number = row * size + column
                 self._gamefield.set_mark(row, column, player.mark)
-            except PlayerInputError as e:
-                print(f'Put digit, which is less {self._gamefield.size ** 2}')
-            except SetMarkError as e:
+            except PlayerInputError as ex:
+                print(ex)
+            except SetMarkError:
                 print('Board[number] must be free')
             else:
                 break
@@ -111,36 +138,36 @@ class TicTacGame:
     def _parse_input(self, player_input: str) -> int:
         if player_input == self.quit_code:
             raise InterruptedGameError()
-        elif self._validate_input(player_input):
-            return int(player_input)
+        return int(self._validate_input(player_input))
 
-    def _validate_input(self, player_input) -> bool:
+    def _validate_input(self, player_input) -> str:
         if (not player_input.isdigit() or
-                not (0 <= int(player_input) < self._gamefield.size ** 2)):
-            raise PlayerInputError(f'{player_input} is not valid. Put digit, which is less {self._gamefield.size ** 2}')
-        else:
-            return True
+                not 0 <= int(player_input) < self._gamefield.size ** 2):
+            raise PlayerInputError(f'{player_input} is not valid. '
+                                   f'Put digit, which is less {self._gamefield.size ** 2}')
+        return player_input
 
     def _show_board(self):
-        print(self._gamefield.show_board())
+        print(self._gamefield.board())
 
     def _show_extended_board(self):
-        print(self._gamefield.show_extended_board())
+        print(self._gamefield.extended_board())
 
-    def _show_greeting(self, winner: TicTacPlayer):
+    @staticmethod
+    def _show_greeting(winner: TicTacPlayer):
         print(f'Player {winner.name} has won!')
 
-    def _show_draw(self):
+    @staticmethod
+    def _show_draw():
         print('Draw')
 
-    def _show_instructions(self):
-        print(f'Type \'{self.quit_code}\' to quit')
+    @staticmethod
+    def _player_input(player):
+        return input(f'{player.name}`s ({player.mark}) move: ').strip()
 
 
 if __name__ == '__main__':
-    name, mark = input('Write first user`s name and mark: name mark (x or o) ').split()
-    user1 = TicTacPlayer(name, mark)
-    name, mark = input('Write first user`s name and mark: name mark (x or o) ').split()
-    user2 = TicTacPlayer(name, mark)
-    game = TicTacGame(user1, user2, 4)
-    game.start_game()
+    name_1 = input('Enter first user`s name: ')
+    name_2 = input('Enter second user`s name: ')
+    game = TicTacGame()
+    game.start_game(name_1, name_2)
---
+++ homework_1/tests.py
@@ -0,0 +1,98 @@
+import unittest
+
+from game import TicTacBoard, TicTacGame
+from exceptions import SetMarkError, PlayerInputError, InterruptedGameError
+
+
+class BoardSetMarkTest(unittest.TestCase):
+    def setUp(self):
+        self.size = 5
+        self.board = TicTacBoard(self.size)
+
+    def test_valid_set(self):
+        with self.subTest():
+            for number in range(self.board.size ** 2):
+                i = number // self.board.size
+                j = number % self.board.size
+                self.board.set_mark(i, j, str(number))
+                self.assertEqual(str(number), self.board._map[i][j])
+
+    def test_invalid_set(self):
+        j = self.board.size - 1
+        less_number = -10
+        larger_number = self.board.size + 10
+        not_number_list = ('1', 'kdslk', [1, 2])
+        with self.subTest():
+            with self.assertRaises(IndexError):
+                self.board.set_mark(less_number, j, 'q')
+                self.board.set_mark(larger_number, j, 'q')
+                self.board.set_mark(j, less_number, 'q')
+                self.board.set_mark(j, larger_number, 'q')
+            with self.assertRaises(TypeError):
+                for obj in not_number_list:
+                    self.board.set_mark(0, obj, 'q')
+                    self.board.set_mark(obj, 0, 'q')
+
+    def test_not_str_set(self):
+        not_str_list = (1, dict(), [1, 2])
+        with self.subTest():
+            with self.assertRaises(SetMarkError):
+                for obj in not_str_list:
+                    self.board.set_mark(0, 0, obj)
+
+    def test_not_free_set(self):
+        i, j = 0, 0
+        self.board.set_mark(i, j, 'no free')
+        with self.assertRaises(SetMarkError):
+            self.board.set_mark(i, j, 'damn')
+
+
+class BoardCheckWinTest(unittest.TestCase):
+    def setUp(self):
+        self.size = 3
+        self.board = TicTacBoard(self.size)
+        self.win_boards = {
+            ((0, 0), (0, 1), (0, 2)): [['x', 'x', 'x'], ['_', '_', '_'], ['_', '_', '_']],
+            ((1, 0), (1, 1), (1, 2)): [['_', '_', '_'], ['x', 'x', 'x'], ['_', '_', '_']],
+            ((2, 0), (2, 1), (2, 2)): [['_', '_', '_'], ['_', '_', '_'], ['x', 'x', 'x']],
+            ((0, 0), (1, 0), (2, 0)): [['x', '_', '_'], ['x', '_', '_'], ['x', '_', '_']],
+            ((0, 1), (1, 1), (2, 1)): [['_', 'x', '_'], ['_', 'x', '_'], ['_', 'x', '_']],
+            ((0, 2), (1, 2), (2, 2)): [['_', '_', 'x'], ['_', '_', 'x'], ['_', '_', 'x']],
+            ((0, 0), (1, 1), (2, 2)): [['x', '_', '_'], ['_', 'x', '_'], ['_', '_', 'x']],
+            ((0, 2), (1, 1), (0, 2)): [['_', '_', 'x'], ['_', 'x', '_'], ['x', '_', '_']]
+        }
+        false_win_boards = []
+
+    def test_true_win(self):
+        with self.subTest():
+            for indexes, board in self.win_boards.items():
+                self.board._map = board
+                for row, column in indexes:
+                    self.assertTrue(self.board.check_win(row, column, 'x'))
+
+    def test_false_win(self):
+        pass
+
+
+class GameParseInputTest(unittest.TestCase):
+    def setUp(self):
+        self.size = 5
+        self.game = TicTacGame(self.size)
+
+    def test_valid_input(self):
+        pl_input = '4'
+        self.assertEqual(self.game._parse_input(pl_input), 4)
+
+    def test_invalid_input(self):
+        pl_input = '5 klop'
+        with self.assertRaises(PlayerInputError):
+            self.game._parse_input(pl_input)
+
+    def test_interrupt(self):
+        pl_input = self.game.quit_code
+        with self.assertRaises(InterruptedGameError):
+            self.game._parse_input(pl_input)
+
+
+if __name__ == '__main__':
+    unittest.main()
