e66bc6bd30f99ea3cacc81668408f080773d80aa
greatjudge <greatjudge0@gmail.com>
2021-10-25 20:32:00 +0400

    changed if condition

--- homework_2/custom_list/custom_list.py
+++ homework_2/custom_list/custom_list.py
@@ -4,20 +4,19 @@ class CustomList(list):
 
     def __add__(self, other):
         self_size, other_size = len(self), len(other)
-        return self.__class__([(self[ind] if ind < self_size else 0) +
-                               (other[ind] if ind < other_size else 0) for ind in range(max(self_size,
-                                                                                          other_size))])
+        return self.__class__([(self[i] if i < self_size else 0) +
+                               (other[i] if i < other_size else 0) for i in range(max(self_size,
+                                                                                      other_size))])
 
     def __radd__(self, other):
         return self + other
 
     def __sub__(self, other):
         self_size, other_size = len(self), len(other)
-        return self.__class__([(self[ind] if ind < self_size else 0) -
-                               (other[ind] if ind < other_size else 0) for ind in range(max(self_size,
-                                                                                          other_size))])
+        return self.__class__([(self[i] if i < self_size else 0) -
+                               (other[i] if i < other_size else 0) for i in range(max(self_size,
+                                                                                      other_size))])
 
     def __rsub__(self, other):
         sub = self - other
         return self.__class__([0] * len(sub)) - sub
-
--- homework_2/custom_meta/custom_meta.py
+++ homework_2/custom_meta/custom_meta.py
@@ -2,7 +2,7 @@ class CustomMeta(type):
     def __new__(mcs, class_name, parents, attributes):
         cls_attrs = {}
         for name, attr in attributes.items():
-            if len(name.split('__')) != 3:
+            if not(name[:2] == '__' and name[-2:] == '__'):
                 name = 'custom_' + name
             cls_attrs[name] = attr
         return super().__new__(mcs, class_name, parents, cls_attrs)
@@ -11,9 +11,8 @@ class CustomMeta(type):
         obj = super().__call__(*args, **kwargs)
         obj_dict = {}
         for name, attr in obj.__dict__.items():
-            if len(name.split('__')) != 3:
+            if not(name[:2] == '__' and name[-2:] == '__'):
                 name = 'custom_' + name
             obj_dict[name] = attr
         obj.__dict__ = obj_dict
         return obj
-
