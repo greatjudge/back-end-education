3ac73b3f961733852ec9e36ed96a59d4c8c48bb3
greatjudge <greatjudge0@gmail.com>
2021-11-15 03:07:14 +0400

    немного переделал models добавил категории

--- quizz/quizz/models.py
+++ quizz/quizz/models.py
@@ -2,17 +2,26 @@ from django.db import models
 from django.urls import reverse
 
 
-# class Category(models.Model):
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Test(models.Model):
-    # creator = on delete set null
+    # creator = models.ForeignKey(User, verbose_name='создатель',
+    #                             on_delete=models.SET_NULL, related_name='answers')
     title = models.CharField(max_length=50, verbose_name='Название')
+    category = models.ManyToManyField(Category, related_name='tests',
+                                      verbose_name='категории теста')
     complexity = models.PositiveSmallIntegerField(blank=True, null=True,
                                                   verbose_name='Сложность')
     description = models.TextField(blank=True, null=True,
                                    verbose_name='Описание теста')
-    datetime = models.DateTimeField(auto_now=True, blank=True)
+    datetime = models.DateTimeField(auto_now=True, blank=True,
+                                    verbose_name='дата и время создания')
 
     def get_absolute_url(self):
         return reverse('tests_detail', kwargs={'test_id': self.id})
@@ -26,11 +35,10 @@ class Test(models.Model):
 
 
 class GradedTest(models.Model):
-    # student = models.ForeignKey()
+    # student = models.ForeignKey(User, verbose_name='пользователь',
+    #                             on_delete=models.CASCADE, related_name='answers')
     test = models.ForeignKey(Test, related_name='grades',
                              on_delete=models.CASCADE, verbose_name='тест')
-    # user_answers = models.ManyToManyField(Choice, on_delete=models.CASCADE,
-    #                                       verbose_name='ответы пользователя')
     grade = models.FloatField()
     datetime = models.DateTimeField(auto_now_add=True)
 
@@ -63,7 +71,7 @@ class Question(models.Model):
                                verbose_name='ответ')
     test = models.ForeignKey(Test, related_name='questions',
                              on_delete=models.CASCADE, verbose_name='тест')
-    # COMING SOON question_type
+    # question_type
 
     def __str__(self):
         return self.title
